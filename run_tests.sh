b2a6ec236a9bfb850046281b093bbe74fbfda367
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-06-06 21:34:45 +0300

    reorder test files

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+for i in {01..10};
+do
+    ./tsp_branch_bound.py "tests/inputs/input_test_$i"
+    #if [ $? -ne 0 ]; then exit $?; fi
+done
+
 for ((i=3;i<=200;i++)); 
 do
-    ./main.py -c$i -w 1 10
+    ./tsp_branch_bound.py -c$i -w 1 10
     if [ $? -ne 0 ]; then exit $?; fi
 done
